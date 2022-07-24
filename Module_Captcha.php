02a6b3231b8e6ef35c73a73790cbab3438a58053
gizmore <gizmore@wechall.net>
2022-07-24 22:13:03 +0200

    '7.0.1-r1401'

--- Module_Captcha.php
+++ Module_Captcha.php
@@ -2,16 +2,16 @@
 namespace GDO\Captcha;
 
 use GDO\Core\GDO_Module;
-use GDO\Core\GDT_Int;
 use GDO\UI\GDT_Color;
 use GDO\UI\GDT_Font;
+use GDO\Core\GDT_UInt;
 
 /**
  * Captcha implementation via PhpCaptcha.php
  * @TODO Add a hidden field captcha_title/ctitle that may not be filled out.
  * 
  * @author gizmore
- * @version 6.10.3
+ * @version 7.0.1
  * @since 3.2.0
  */
 final class Module_Captcha extends GDO_Module
@@ -24,14 +24,14 @@ final class Module_Captcha extends GDO_Module
 		return [
 			GDT_Font::make('captcha_font')->multiple()->minSelected(1)->initialValue(["GDO/Core/thm/default/fonts/arial.ttf"])->notNull(),
 			GDT_Color::make('captcha_bg')->initial('#f8f8f8')->notNull(),
-			GDT_Int::make('captcha_width')->initial('256')->min(48)->max(512)->notNull(),
-			GDT_Int::make('captcha_height')->initial('48')->min(24)->max(256)->notNull(),
+			GDT_UInt::make('captcha_width')->initial('256')->min(48)->max(512)->notNull(),
+			GDT_UInt::make('captcha_height')->initial('48')->min(24)->max(256)->notNull(),
 		];
 	}
-	public function cfgCaptchaFonts() { return $this->getConfigValue('captcha_font'); }
-	public function cfgCaptchaBG() { return $this->getConfigValue('captcha_bg'); }
-	public function cfgCaptchaWidth() { return $this->getConfigValue('captcha_width'); }
-	public function cfgCaptchaHeight() { return $this->getConfigValue('captcha_height'); }
+	public function cfgCaptchaFonts() : array { return $this->getConfigValue('captcha_font'); }
+	public function cfgCaptchaBG() : string { return $this->getConfigValue('captcha_bg'); }
+	public function cfgCaptchaWidth() : int { return $this->getConfigValue('captcha_width'); }
+	public function cfgCaptchaHeight() : int { return $this->getConfigValue('captcha_height'); }
 	
 	public function onIncludeScripts() : void
 	{
