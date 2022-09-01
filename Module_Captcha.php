b231a3f56951656e4efa5c6414f1386234c8b564
gizmore <gizmore@wechall.net>
2022-09-01 11:15:59 +0200

    '7.0.1-r1560 tests'

--- Module_Captcha.php
+++ Module_Captcha.php
@@ -18,11 +18,13 @@ final class Module_Captcha extends GDO_Module
 {
     public string $license = 'BSD';
     
-	public function onLoadLanguage() : void { $this->loadLanguage('lang/captcha'); }
+    ##############
+    ### Config ###
+    ##############
 	public function getConfig() : array
 	{
 		return [
-			GDT_Font::make('captcha_font')->multiple()->minSelected(1)->initialValue(["GDO/Core/thm/default/fonts/arial.ttf"])->notNull(),
+			GDT_Font::make('captcha_font')->multiple()->minSelected(1)->initial($this->getInitialFontsVar())->notNull(),
 			GDT_Color::make('captcha_bg')->initial('#f8f8f8')->notNull(),
 			GDT_UInt::make('captcha_width')->initial('256')->min(48)->max(512)->notNull(),
 			GDT_UInt::make('captcha_height')->initial('48')->min(24)->max(256)->notNull(),
@@ -33,9 +35,33 @@ final class Module_Captcha extends GDO_Module
 	public function cfgCaptchaWidth() : int { return $this->getConfigValue('captcha_width'); }
 	public function cfgCaptchaHeight() : int { return $this->getConfigValue('captcha_height'); }
 	
+	#############
+	### Hooks ###
+	#############
+	public function onLoadLanguage() : void
+	{
+		$this->loadLanguage('lang/captcha');
+	}
+
 	public function onIncludeScripts() : void
 	{
 	    $this->addCss('css/captcha.css');
+	}
+
+	###############
+	### Private ###
+	###############
+	private function getInitialFontsVar() : string
+	{
+		return '["arial","kitten","kitten2","microgramma","teen"]';
+// 		$f = 'GDO/Core/thm/default/fonts/';
+// 		return [
+// 			"{$f}arial.ttf",
+// 			"{$f}kitten.ttf",
+// 			"{$f}kitten2.ttf",
+// 			"{$f}microgramma.ttf",
+// 			"{$f}teen.ttf",
+// 		];
 	}
 
 }
